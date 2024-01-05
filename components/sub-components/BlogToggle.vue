ef4d80ec24158451957d0202ea10068544549617
vaneath <vaneathzz@gmail.com>
2024-01-05 21:27:02 +0700

    close #6 add work place and blog toggle in homepage

---
+++ components/sub-components/BlogToggle.vue
@@ -0,0 +1,28 @@
+<
+<template>
+  <div>
+    <nuxt-link
+      to="/blogs"
+      class="group inline-flex hover:bg-black items-center text-white bg-gray-900 rounded-full p-0.5 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
+    >
+      <span
+        class="px-3 py-0.5 text-white text-xs font-semibold leading-5 tracking-wide bg-indigo-600 rounded-full"
+        >i write blog</span
+      >
+      <span class="ml-4 text-xs">have a look</span>
+      <svg
+        class="transition duration-500 transform block group-hover:rotate-180 hover-arrow ml-2 w-5 h-5 text-gray-500"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </nuxt-link>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,11 @@
 <template>
   <div>
-    <HeroSection />
-    <!-- <GithubCalendar v-if="$config.social.github" />
-    <Recommendations v-if="$config.recommendations.enabled" />
-    <LazyRecentBlog v-if="$config.blog.enabled && posts" :posts="posts" /> -->
+    <HeroSection v-if="config.heroSection.enabled" />
   </div>
 </template>
 
-<script>
-export default {
-  head() {
-    return {
-      title: `home -- ${this.$config.name}`,
-    };
-  },
-  data() {
-    return {
-      posts: null,
-    };
-  },
-  async created() {
-    const fetchDocsLabel = "fetchAllPosts";
-    console.time(fetchDocsLabel);
-    try {
-      const posts = await this.$content("posts")
-        .without(["body", "toc", "dir", "extension", "path", "tags"])
-        .limit(3)
-        .skip(0)
-        .sortBy("createdAt", "desc")
-        .fetch();
-      this.posts = posts;
-    } catch (e) {
-      console.error(e);
-    } finally {
-      console.timeEnd(fetchDocsLabel);
-    }
-  },
-};
+<script setup>
+const config = useAppConfig();
 </script>
 
 <style></style>
