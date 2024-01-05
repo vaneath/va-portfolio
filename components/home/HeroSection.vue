0963e071c5a9f32630f17bedb8575a4c4c2c7db4
vaneath <vaneathzz@gmail.com>
2024-01-06 00:28:56 +0700

    close #17 change app dependencies

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -14,7 +14,10 @@
                 <h1
                   class="mt-4 text-3xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-3xl xl:text-3xl"
                 >
-                  <span class="md:block" v-text="config.heroSection.title"></span>
+                  <span
+                    class="md:block"
+                    v-text="config.heroSection.title"
+                  ></span>
                 </h1>
                 <p
                   class="mt-3 text-base text-gray-300 sm:mt-5"
@@ -30,7 +33,9 @@
                   v-if="config.heroSection.workedAt.enabled"
                   class="mt-5 w-full sm:mx-auto lg:ml-0"
                 >
-                  <div class="flex flex-wrap space-x-1 items-center md:space-x-10">
+                  <div
+                    class="flex flex-wrap space-x-1 items-center md:space-x-10"
+                  >
                     <div
                       v-for="(firm, index) in config.heroSection.workedAt.meta"
                       :key="index"
@@ -56,7 +61,11 @@
               <div
                 class="flex-shrink-0 w-full sm:mx-auto flex flex-1 items-center justify-center rounded-lg sm:overflow-hidden"
               >
-                <img class="rounded-md h-80" :src="config.image" :alt="config.name" />
+                <img
+                  class="rounded-md h-80"
+                  :src="config.image"
+                  :alt="config.name"
+                />
               </div>
             </div>
           </div>
