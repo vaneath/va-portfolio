c597c2337162e44f3c99f60bd8d469bb5be3285b
vaneath <vaneathzz@gmail.com>
2024-01-06 17:52:21 +0700

    close #30 add favicon and page title

--- components/sub-components/AboutMe.vue
+++ components/sub-components/AboutMe.vue
@@ -3,10 +3,7 @@
     <p class="mt-8 text-sm text-white lowercase tracking-wide font-semibold sm:mt-10">
       about me
     </p>
-    <p
-      class="mt-3 text-base text-gray-300 sm:mt-5"
-      v-text="config.heroSection.description"
-    ></p>
+    <p class="mt-3 text-base text-gray-300 sm:mt-5" v-text="config.description"></p>
   </div>
 </template>
 
