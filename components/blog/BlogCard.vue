9221e9fc3be70d7b04602dd60634612e578f8bb5
vaneath <vaneathzz@gmail.com>
2024-01-11 22:44:56 +0700

    close #42 draft blog layout

---
+++ components/blog/BlogCard.vue
@@ -0,0 +1,76 @@
+
+import type { set } from 'nuxt/dist/app/compat/capi';
+<template>
+  <nuxt-link
+    to="/blogs/1"
+    class="flex transition duration-700 ease-in-out transform hover:translate-y-0.5 hover:scale-105 flex-col rounded-xl shadow-lg overflow-hidden"
+  >
+    <div v-if="config.image" class="hidden sm:flex flex-shrink-0 bg-gray-900">
+      <img class="h-36 w-full object-cover" :src="config.image" alt="Blog Image" />
+    </div>
+    <div class="flex-1 bg-gray-900 p-5 flex flex-col justify-between">
+      <div class="flex-1">
+        <p class="text-xs font-medium text-hot-pink hover:text-hot-pink">
+          <a href="#" class="hover:underline hover:text-hot-pink">
+            category
+          </a>
+        </p>
+        <div>
+          <h3 class="mt-0.5 text-md leading-7 font-semibold text-gray-200">
+            {{ config.name }}
+          </h3>
+          <p class="mt-2 text-xs text-gray-400">
+            {{ config.description }}
+          </p>
+        </div>
+      </div>
+      <div class="mt-2 flex items-center">
+        <div class="flex-shrink-0">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://twitter.com/vaneath"
+          >
+            <UserAvatar
+              class="h-9 w-9 rounded-full"
+              name="vaneath"
+            :photo-u-r-l="config.image"
+            />
+          </a>
+        </div>
+        <div class="ml-3">
+          <p class="text-xs font-medium text-gray-200">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://twitter.com/vaneath"
+              class="hover:text-yellow-600"
+            >
+              author name
+            </a>
+          </p>
+          <!-- <div class="flex text-xs text-gray-300">
+            <time :datetime="post.createdAt">
+              {{
+                new Date(post.createdAt).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })
+              }}
+            </time>
+            <span class="mx-1"> &middot; </span>
+            <span>
+              reading time
+            </span>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script setup>
+const config = useAppConfig();
+</script>
+
--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <ComingSoon />
+  <div
+    class="relative max-w-7xl border-r border-l border-dashed border-gray-700 py-6 mx-auto min-h-screen"
+  >
+    <div data-aos="fade-up" class="text-center">
+      <h2 class="text-2xl tracking-tight font-extrabold text-gray-200">
+        blog header
+      </h2>
+      <p class="mt-2 px-2 max-w-2xl mx-auto text-sm leading-7 text-gray-400">
+        blog subtext
+      </p>
+    </div>
+
+    <div
+      data-aos="zoom-in"
+      class="select-none px-4 items-center justify-center sm:justify-start overflow-hidden flex pt-4"
+    >
+      <nav
+        class="flex flex-wrap items-center justify-center flex-row space-x-2 sm:space-x-4"
+        aria-label="Tabs"
+      >
+        <button
+          class="flex text-gray-300 focus:outline-none focus:ring-transparent focus:ring-offset-transparent hover:text-hot-pink px-3 py-2 font-medium text-sm rounded-xl"
+        >
+          category
+        </button>
+      </nav>
+    </div>
+
+    <div data-aos="zoom-in" class="mt-5 gap-4 mx-4 grid max-w-none lg:grid-cols-3">
+      <BlogCard />
+    </div>
   </div>
 </template>
 
